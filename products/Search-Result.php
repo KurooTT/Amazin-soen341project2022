fc8e2a2776b8382b6f0e8446a49e48c8456eff16
Julie.T <minato_fan_@hotmail.com>
2022-04-16 19:52:26 -0400

    Cleaned code

--- products/Search-Result.php
+++ products/Search-Result.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang = "en">
+<html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,27 +9,30 @@
     <link rel="stylesheet" href="../Application/css/product-landing.css" />
     <title>search</title>
 </head>
+
 <body>
-<?php
-include "../navbar.php";
+    <?php
+    include "../navbar.php";
 
-$search = isset($_POST['search']) ? $_POST['search'] : '';
-$search = ucfirst($search);
-$db= mysqli_connect("localhost", "root","321trewq", "amazin","3306") or die ("fail");
-$query="SELECT products.id, name, price, username , description,imgPath, sellerID  FROM products,customers where (description LIKE '%$search%' OR name LIKE '%$search%'
-OR category LIKE '%$search%') AND products.sellerID = customers.id" ;
-$result=mysqli_query($db,$query);
+    //DATABASE CONNECTION
+    $search = isset($_POST['search']) ? $_POST['search'] : '';
+    $search = ucfirst($search);
+    $db = mysqli_connect("localhost", "root", "321trewq", "amazin", "3306") or die("fail");
+    $query = "SELECT products.id, name, price, username , description,imgPath, sellerID  FROM products,customers where (description LIKE '%$search%' OR name LIKE '%$search%'
+OR category LIKE '%$search%') AND products.sellerID = customers.id";
+    $result = mysqli_query($db, $query);
 
 
-?>
-<h1>Search Results</h1>
-<h6><?php echo mysqli_num_rows($result) ;?> result(s) </h6>
+    ?>
+    <h1>Search Results</h1>
+    <h6><?php echo mysqli_num_rows($result); ?> result(s) </h6>
 
-<?php
-while($row = mysqli_fetch_assoc($result)) {
-    $img = "<img class=\"landing-item_img\"  src=\"../" . $row['imgPath'] . "\"alt=\"" .  "\" />";
-    $linkPath = "product-detail.php?code=" . $row['id'];
-    $item = "<a class=\"landing-item__link\" href=\"" . $linkPath . "\">
+    <?php
+    //FETCH DATA AND DISPLAY ON PAGE
+    while ($row = mysqli_fetch_assoc($result)) {
+        $img = "<img class=\"landing-item_img\"  src=\"../" . $row['imgPath'] . "\"alt=\"" .  "\" />";
+        $linkPath = "product-detail.php?code=" . $row['id'];
+        $item = "<a class=\"landing-item__link\" href=\"" . $linkPath . "\">
 <div class=\"landing-item\">
     <div class=\"landing-item_img-wrapper\">
         " . $img . "
@@ -56,7 +60,7 @@ while($row = mysqli_fetch_assoc($result)) {
                 <p style='float: right' class=\"item--price\">
                    seller: " . $row['username'] . "<br>
                    seller ID: " . $row['sellerID'] . "<br>
-                   product id: ".$row['id']."
+                   product id: " . $row['id'] . "
                 </p>
             </div>
         
@@ -64,12 +68,9 @@ while($row = mysqli_fetch_assoc($result)) {
     </div>
 </div>
 </a>";
-    echo $item;
-}
-include('../footer.php');
+        echo $item;
+    }
+    include('../footer.php');
 
-?>
+    ?>
 </body>
-
-
-
